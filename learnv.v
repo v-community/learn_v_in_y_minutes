b254725b4904dbb13c6135a17db26c7acd3a781a
Andrew Lesh <alesh@gmail.com>
2019-10-01 21:03:01 -0400

    updated spacing and introduced multiple returns

--- learnv.v
+++ learnv.v
@@ -40,7 +40,6 @@ const (
     structs, like in C, allow you define a group of different data-types together in a single, logical type
     more advanced features will be covered shortly
 */
-
 struct Address {
     pub:
         street string
@@ -48,6 +47,7 @@ struct Address {
         state string
         zip int
 }
+
 /* 
     There can be multiple constant declarations throughout source code; 
     although it is recommended to declare as many as possible in the same area 
@@ -61,7 +61,7 @@ const (
 
 /*
     Function declarations follow many other languages' form:    
-        fn function_name(param_list) return_type {
+        fn function_name(param_list) return_type_list {
             function_body
         }
 */
@@ -99,41 +99,54 @@ struct Point {
 fn test_out_of_order_calls() {
     // unlike most languages, variables can only be defined in a function scope
     point := Point{x_coor : 2, y_coor : 2}
+    
     // Variables are immutable by default
     mut point1 := Point{}
+    
     // := is used for initialization, = is an assignment
     point1 = Point{x_coor : 1, y_coor : 1}    
+    
+    x_diff, y_diff, distance := point.dist(point1)
+    
     // A function can be used before their declaration to alleviate the need for header files
-    println(point.dist(point1))
+    println('difference in:\nx_coors = $x_diff, y_coors = $y_diff\nthe distance between them is ${distance:.2f}')
 }
 
-fn (p Point) dist(p2 Point) string {  
+fn (p Point) dist(p2 Point) (f64, f64, f64)  {  
     // you can perform type conversion with the T(v) form
     // the following is int => f64 using the form f64(int)
     x_diff_immutable := f64(p2.x_coor - p.x_coor)    
+    
     // x_diff_immutable = 2 would cause a compile error (test it, I'll wait ;] )    
     mut y_diff_mutable := f64(p2.y_coor - p.y_coor)
+    
     // as you've realized now, the mut keyword denotes that a variable should be mutable    
     mut distance := math.pow(x_diff_immutable, 2)    
-    y_diff_mutable = math.pow(y_diff_mutable, 2)    
+    y_diff_mutable = math.pow(y_diff_mutable, 2)   
+    
     // that allows us to assign a new value to a variable after it's initialized
     distance = distance + y_diff_mutable    
     distance = math.sqrt(distance)
+    
     // you could obviously do : distance = math.sqrt(distance + y_diff_mutable)
-    return 'difference in:\nx_coors = $x_diff_immutable, y_coors = $y_diff_mutable\nthe distance between them is ${distance:.2f}'
+    return x_diff_immutable, y_diff_mutable, distance
 }
 
 fn string_example() {
     // a char is denoted by a set of backticks ( ` )  (on many PCs this is the key under escape)
     a_char := `a`
+    
     // you've seen examples, but interpolated strings are readily available 
     println('The ascii value of this char is: $a_char')
+    
     // basic values can be interpolated directly,
     // more advanced interpolations require ${to_be_interpolated}
     println('The char is: ${a_char.str()}')
+    
     // if you prefer, concatenation is always available
     mut concat := 'b'+a_char.str()+'dnews be'+a_char.str()+'rs'
     print(concat)
+    
     // use += to append to a string
     concat += '_appended'
     println(', $concat')
@@ -142,13 +155,17 @@ fn string_example() {
 fn arrays_example() {
     // arrays are collections of a SINGLE data type
     mut fruits := ['apple', 'banana', 'cherry']
+    
     // the data type is determined by the type of the first element that it contains
     println(fruits)
+    
     // use << to append to the end 
     fruits << 'kiwi'
     println(fruits)
+    
     // arrays can be pre-allocated
     ben_10 := ['ben'].repeat(10)
+    
     // use .len to get the number of elements in an array
     // use array_name[desired_index] to get the element at a specific index (indices start at 0)
     println('There are ${ben_10.len} occurrences of ${ben_10[0]} in \n'+ben_10.str())
@@ -184,19 +201,19 @@ fn maps_example() {
 */
 fn conditional_example() {
     a := 15
-    b := 35    
+    b := 35  
+    
     // parentheses around the condition can be useful for longer expressions
     if (b == 2*a) {
         println('b ($b) is twice the value of a ($a)')
-    }
-    // however they are not required
-    else if a > b {
+    }  
+    else if a > b { // however they are not required
         println('a ($a) is greater than b ($b)')
-    }
-    // the curly braces are though
-    else {
+    }      
+    else { // the curly braces are though
         println('a ($a) is less than or equal to b ($b)')
     }
+    
     // if-else suites can be used as an expression and the result stored in a variable
     mult_of_3 := if a % 3 == 0 {
         'a ($a) is a multiple of 3'
@@ -205,8 +222,10 @@ fn conditional_example() {
         'a ($a) is NOT a multiple of 3'
     }    
     println(mult_of_3)    
-    c := `c`
+    
+    c := `c` // cchange this to see other results
     mut x := ''    
+    
     // a shorthand to if c == `a` is v's match statement
     // it is similar to many other languages' switch statement
     match c {
@@ -234,6 +253,7 @@ fn conditional_example() {
 */
 fn in_example() {
     arr := [1,2,3,5]
+    
     // for arrays, 'in' checks if a specified element is a value stored in it
     x := if 4 in arr {
             'There was a 4 in the array'
@@ -244,6 +264,7 @@ fn in_example() {
     println(x)
     
     m := {'ford' : 'mustang', 'chevrolet' : 'camaro', 'dodge' : 'challenger'}
+    
     // for maps, 'in' checks if a specified element is a key of the map
     y := if 'chevrolet' in m {
             'The chevrolet in the list is a '+m['chevrolet']
@@ -257,13 +278,16 @@ fn in_example() {
 fn (m map[string]f64 ) str() string {
     mut result := ''
     // V has no while loop, for loops have several forms that can be utilized
+    
     mut count := 0
     num_keys := m.size
     println('Number of keys in the map $num_keys')
+    
     // the basic for loop will run indefinitely
     for {
         count += 2            
         println(num_keys.str())
+        
         if count == num_keys - 1 {
             // until it reaches a break statement...or the comp runs out of resources :]
             break 
@@ -271,25 +295,31 @@ fn (m map[string]f64 ) str() string {
         else if (count == 6){
             // continue statements skip to the next iteration of the loop
             continue
-        }        
+        }   
+        
         result += 'Count is $count'            
     }
+    
     // the more standard for loop is available as well    
     for i := 1; i <= 10; i++ {
         if i % 2 == 0 {
             println('i ($i) is even')
         }
     }
+    
     // the for...in... acts like the foreach of most languages
     for val in [1,2,3] {
         result += '$val '
     }
+    
     result += '\n'
+    
     // the for key, val in... is a specialized version of the 'foreach' loop
     for key, val in m {
         result += 'key: $key -> value: $val '
     }
-    // this one is very handy for maps or when the index in arrays is needed
+    
+    // the last one is very handy for maps or when the index in arrays is needed
     
     return result
 }
@@ -300,11 +330,13 @@ fn (m map[string]f64 ) str() string {
 fn defer_example() {
     mut a := f64(3)
     mut b := f64(4)
+    
     // anything within this block won't run until the code after it has completed
     defer {
         c := math.sqrt(a+b)
         println('The hypotenuse of the triangle is $c')
     }
+    
     // this should be executed before the statements above
     a = math.pow(a, 2)
     b = math.pow(b, 2)
@@ -335,7 +367,10 @@ fn error_handling_example() {
     fail := divide(x, y) or {
         // err is a special value for the 'or' clause that corresponds to the text in the error statement
         println(err)
-        return
+        return 
+        
+        // or blocks must end with a return, break, or continue statements.
+        // the last two (break and continue) must be in a for loop of some kind
     }
     
     /*
